cac80f3acde22b13405cfdeb3740bd08573675e7
1797158615 <1797158615@qq.com>
2025-02-25 02:02:02 +0800

    参数

--- codegen.cpp
+++ codegen.cpp
@@ -16,9 +16,16 @@ int main(int argc, char* argv[])
     }
     
     string path = "./ta_server.c";
+    string comm = argv[2];
+    if(argc > 3) {
+    	for(int i = 3; i < argc; i++){
+    		comm += " ";
+    		comm += argv[i];
+    	}
+    }
 
     // 创建 宿主机对象
-    ta_server_gen_class ta_server(argv[1]);
+    ta_server_gen_class ta_server(comm);
     
     // 使用 ofstream 进行文件操作
     ofstream ofs(path);  // 打开文件
@@ -34,7 +41,7 @@ int main(int argc, char* argv[])
     //生成shell
     path = "./ta_setup";
     
-    td_setup_gen_class td_setup(argv[2]);
+    td_setup_gen_class td_setup(argv[1]);
     ofstream td(path);
     if (!td) {  // 检查文件是否成功打开
         cerr << "无法打开文件: " << path << endl;
